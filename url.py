6b12abdf3e49fdc3e8c5ef2edec25cbc91b66c1b
root <root@AlejoThinkPad>
2024-01-23 17:59:23 +0100

    first commit

---
+++ url.py
@@ -0,0 +1,18 @@
+import url_library
+bad_url = "http://malware.testing.google.test/testing/malware/"
+good_url = "https://www.google.com"
+special_url = "http://malware.testing.googвle.test/testing/malware/"
+
+
+
+is_domain_listed = url_library.get_domain_safe_info(bad_url)
+print(is_domain_listed)
+
+domain_age  = url_library.get_domain_time_info(good_url)
+print(domain_age)
+
+#isIp = url_library.is_ip(bad_url)
+#print(isIp)
+
+#specialCharacter = url_library.special_characters(bad_url)
+#print(specialCharacter)
---
+++ url_library.py
@@ -0,0 +1,110 @@
+
+import yaml
+import requests
+import json
+import re
+from urllib.parse import urlparse
+
+with open('./config/conf.yml', 'r') as file:
+    config = yaml.safe_load(file)
+#Api Safe Browsing 
+#Pasamos una URL obtenemos de la api de safe browsing si está listada como maliciosa o no
+def get_domain_safe_info(url):
+    #API_KEY = 'AIzaSyCS2NbAvcd5QbT0knabmtMbfPyK3roHiPA'
+    # URL de la API de Safe Browsing
+    api_url = 'https://safebrowsing.googleapis.com/v4/threatMatches:find'
+    # Parámetros de la petición
+    payload = {
+        'client': {
+            'clientId': "tu_cliente",
+            'clientVersion': "1.5.2"
+        },
+        'threatInfo': {
+            'threatTypes':      ["MALWARE", "SOCIAL_ENGINEERING", "UNWANTED_SOFTWARE", "POTENTIALLY_HARMFUL_APPLICATION"],
+            'platformTypes':    ["ANY_PLATFORM"],
+            'threatEntryTypes': ["URL"],
+            'threatEntries': [
+                {"url": url}
+            ]
+        }
+    }
+    try:
+        response = requests.post(api_url, params={'key': config["safe_browsing_api_key"]}, json=payload)
+        response.raise_for_status()
+        result = response.json()
+        if result:
+            return 1
+    except requests.exceptions.HTTPError as err:
+        print(f'Error en la petición HTTP: {err}')
+    except Exception as e:
+        print(f'Error: {e}')
+#Antiguedad del Dominio
+#Recibe un string con el nombre de dominio y hace una petición a la api ip2whois.com
+def get_domain_time_info(domain):
+    API_KEY = "F786254BBEFC579B0790903539EB6D56"
+    #api_url = "https://api.ip2whois.com/v2?key=" + API_KEY + "&domain=" + domain
+    try:
+        response = requests.get("https://api.ip2whois.com/v2", params={'key': config["doman_info_api_key"], "domain": domain})
+        response.raise_for_status()
+        result = response.json()
+        if result["domain_age"]:
+            return result["domain_age"]
+    except requests.exceptions.HTTPError as err:
+        print(f'Error en la petición HTTP: {err}')
+    except Exception as e:
+        print(f'Error: {e}')
+
+#Es una IP
+def is_ip(url):
+    pattern = r'\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b'
+    if re.search(pattern, url):
+        return 1
+    else:
+        return 0
+#Url Length
+def url_length(url):
+    return len(url)
+#Cantidad subdominios
+def number_of_subdomains(url):
+    domain = urlparse(url).netloc
+    parts = domain.split('.')
+    # La cantidad de subdominios será el número de partes menos 2 (dominio y TLD)
+    # Pero puede variar si se trata de un TLD de segundo nivel como .co.uk
+    if len(parts) > 2:
+        # Aquí se pueden agregar condiciones para TLDs especiales
+        if parts[-2] in ['co', 'com', 'org', 'net']:  # Ejemplo para TLDs de segundo nivel
+            return len(parts) - 3
+        else:
+            return len(parts) - 2
+    else:
+        return 0
+#HTTPS
+def is_https(url):
+    result = urlparse(url)
+    if result.scheme == 'https':
+        return 1
+
+#Carácteres Especiales
+def special_characters(url):
+    pattern = re.compile(r"[^a-zA-Z0-9/:.?#&=_%-]")
+    if pattern.search(url):
+        return 1
+    else:
+        return 0
+
+#Dominio Sospechoso
+def tlds_blacklist(url):
+    tlds_blacklist = ['.tk', '.ml', '.ga', '.cf', '.gq', '.xyz', '.info', '.ru', '.cn']
+    tld = '.' + urlparse(url).netloc.split('.')[-1]
+    return tld in tlds_blacklist
+#Hace una redirección
+def make_redirection(url):
+    try:
+        response = requests.get(url)
+        if response.history:
+            return 1
+        else:
+            return 0
+    except requests.RequestException as e:
+        print(f"Error al realizar la solicitud: {e}")
+        return False
